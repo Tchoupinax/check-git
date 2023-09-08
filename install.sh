0c8cd2428eec14ef6ed5f3009441c81cf7b60fc3
Tchoupinax <corentinfiloche@hotmail.fr>
2023-09-08 14:54:41 +0200

    chore: deploy linux script installation

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-
 LINUX=false
 OS="$(uname)"
 if [[ "${OS}" == "Linux" ]]; then
@@ -7,4 +6,11 @@ fi
 
 UNAME_MACHINE="$(/usr/bin/uname -m)"
 
-if [[ UNAME_MACHINE == "x86_64" && LINUX ]]; then
+echo $LINUX
+echo $UNAME_MACHINE
+
+if [[ $UNAME_MACHINE == "x86_64" ]] && [[ $LINUX == true ]]; then
+  wget https://github.com/Tchoupinax/check-git/releases/download/v0.2.0/check-git_0.2.0_linux_amd64.tar.gz &> /dev/null
+  tar xvf check-git_0.2.0_linux_amd64.tar.gz
+  rm check-git_0.2.0_linux_amd64.tar.gz
+fi
